3d1daf170588b8b32021a21f7a6c3b01dec73842
Dave Nottage <davidn@radsoft.com.au>
2022-09-01 09:46:07 +0930

    Fix AppStoreCheck demo

--- Demos/AppVersionCheck/AppVersionCheckDemo.dpr
+++ Demos/AppVersionCheck/AppVersionCheckDemo.dpr
@@ -3,8 +3,7 @@ program AppVersionCheckDemo;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Unit1 in 'Unit1.pas' {Form1},
-  DW.AppStoreCheck in 'Z:\Kastri\Core\DW.AppStoreCheck.pas';
+  Unit1 in 'Unit1.pas' {Form1};
 
 {$R *.res}
 
